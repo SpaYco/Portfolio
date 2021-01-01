3aa08fe87aa0fdfed7a70926e6f7e6381e882e67
SpaYco <zizou.aziz101@gmail.com>
2021-01-01 23:14:05 +0100

    Improved nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,17 +7,29 @@ import { Link } from 'react-scroll';
 const Nav = ({ toggleMenu }) => (
   <nav className={`${toggleMenu ? 'show-menu' : ''}`}>
     <ul>
-      <li name="About Me">
-        <Link activeClass="active" to="about" spy smooth offset={50} duration={500}><FontAwesomeIcon icon={faIdCard} /></Link>
+      <li>
+        <Link activeClass="active" to="about" spy smooth offset={50} duration={500}>
+          <FontAwesomeIcon icon={faIdCard} />
+          <p>About Me</p>
+        </Link>
       </li>
-      <li name="Projects">
-        <Link activeClass="active" to="projects" spy smooth offset={50} duration={500}><FontAwesomeIcon icon={faCode} /></Link>
+      <li>
+        <Link activeClass="active" to="projects" spy smooth offset={50} duration={500}>
+          <FontAwesomeIcon icon={faCode} />
+          <p>Projects</p>
+        </Link>
       </li>
-      <li name="Resume">
-        <Link activeClass="active" to="resume" spy smooth offset={50} duration={500}><FontAwesomeIcon icon={faFile} /></Link>
+      <li>
+        <Link activeClass="active" to="resume" spy smooth offset={50} duration={500}>
+          <FontAwesomeIcon icon={faFile} />
+          <p>Resume</p>
+        </Link>
       </li>
-      <li name="Contact Me">
-        <Link activeClass="active" to="contact" spy smooth offset={50} duration={500}><FontAwesomeIcon icon={faAt} /></Link>
+      <li>
+        <Link activeClass="active" to="contact" spy smooth offset={50} duration={500}>
+          <FontAwesomeIcon icon={faAt} />
+          <p>Contact Me</p>
+        </Link>
       </li>
     </ul>
   </nav>
