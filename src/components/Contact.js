cffdd0647dbb37d19bcccfd56bf09f9203757392
SpaYco <zizou.aziz101@gmail.com>
2021-01-02 01:39:33 +0100

    added contact section and final touches

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithubAlt,
+  faTwitter,
+  faLinkedin,
+  faAngellist,
+} from '@fortawesome/free-brands-svg-icons';
+import Title from './Title';
+
 const Contact = () => (
   <div id="contact" className="card">
-    <p>Hello</p>
+    <Title title="Contact" />
+    <div id="contact-list">
+      <a href="mailto:zizou.aziz101@gmail.com" className="contact-btn" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faEnvelope} />
+        {' '}
+        Email
+      </a>
+      <a href="link.com" className="contact-btn" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faGithubAlt} />
+        {' '}
+        GitHub
+      </a>
+      <a href="link.com" className="contact-btn" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} />
+        {' '}
+        LinkedIn
+      </a>
+      <a href="link.com" className="contact-btn" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faTwitter} />
+        {' '}
+        Twitter
+      </a>
+      <a href="link.com" className="contact-btn" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faAngellist} />
+        {' '}
+        AngelList
+      </a>
+    </div>
   </div>
 );
 
