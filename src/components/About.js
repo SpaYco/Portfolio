3a5f6bd282e1e3cac8b6748dc07c6413f0cdb36c
SpaYco <zizou.aziz101@gmail.com>
2021-01-07 01:43:02 +0100

    color and grammar fixes

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import Contact from './Contact';
 import Title from './Title';
 
-const skills = ['Front-end development: ReactJS', 'Back-end development: Ruby on Rails', 'Databases: MySQL | PostgreSQL', 'Infra: Netlify | Heroku', 'Styling: CSS| SASS | MaterializeCSS | Bootstrap', 'Remote and Collaborative work experience'];
+const skills = ['Front-End development: ReactJS', 'Back-End development: Ruby on Rails', 'Databases: MySQL | PostgreSQL', 'Infra: Netlify | Heroku', 'Styling: CSS| SASS | MaterializeCSS | Bootstrap', 'Remote and Collaborative work experience'];
 
 const About = () => (
   <div id="about" className="card">
     <Title title="About Me" />
     <p className="description">
-      I&apos;m a skilled Full-Stack Web Developer who loves making pages with beautiful UI.
+      I&apos;m a skilled full-stack web developer who loves making pages with beautiful UI.
       <br />
       I studied at Microverse and I am experienced in remote work.
       <br />
